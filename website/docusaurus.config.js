8989d7bad7c96ba440344b1fa96af1a811da97d1
vitor feijo leonardo <vitorfleonardo@gmail.com>
2024-05-12 18:21:39 -0300

    fix

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/UnB-Software-Engineering-Course/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'vitorfleonardo', // Usually your GitHub org/user name.
   projectName: 'UnB-Software-Engineering-Course', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
