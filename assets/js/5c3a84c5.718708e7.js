57848f1d2100ed5e36b7934d483e26a4fd802ec8
vitor feijo leonardo <vitorfleonardo@gmail.com>
2024-05-12 18:34:00 -0300

    Updates

---
+++ assets/js/5c3a84c5.718708e7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[139],{9461:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/UnB-Software-Engineering-Course/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
